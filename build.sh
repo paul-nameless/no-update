56d2abf463d358f08d9bc7c43173d1e721433870
Paul Nameless <reacsdas@gmail.com>
2026-03-08 19:07:09 +0100

    Add icon

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 set -e
 swiftc main.swift -o NoUpdate
 mkdir -p NoUpdate.app/Contents/MacOS
+mkdir -p NoUpdate.app/Contents/Resources
 mv NoUpdate NoUpdate.app/Contents/MacOS/
 cp Info.plist NoUpdate.app/Contents/
+cp logo.png NoUpdate.app/Contents/Resources/
 codesign -f -s - NoUpdate.app
 echo "Built NoUpdate.app"
